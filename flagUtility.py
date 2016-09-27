bbec9ce3378d41f577c5e8db4725d5333846ee82
Ryan <tenghaow@andrew.cmu.edu>
2016-09-26 20:28:56 -0700

    sractch
    flag utility and rigging utility

---
+++ flagUtility.py
@@ -0,0 +1,25 @@
+import maya.cmds as maya 
+import pymel.core as pm
+
+
+class flagUtitlty(object):
+
+	def circleWire_Y(self):
+		circle = pm.circle()
+
+	def circleWire_X(self):
+		circle = pm.circle()
+		makecirclenode = pm.nodetypes.MakeNurbCircle(circle[1])
+		makecirclenode.setNormal([1,0,0])
+
+
+	def circleWire_Z(self):
+		circle = pm.circle()
+
+
+	def cubeWire(self):
+		print ('cube flag generated')
+
+
+test = flagUtitlty()
+test.circleWire_X()
