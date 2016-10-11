3f91f8c759b018c5db2ddacfaeb3a0bd5ad316c2
Ryan <tenghaow@andrew.cmu.edu>
2016-10-11 15:35:02 -0700

    Revert "Revert "Rigging frame""
    This reverts commit bae8ab0adf504899ad5b0f117651ce89fa077cfc.

--- RiggingTools/RiggingTools/flagUtility.py
+++ RiggingTools/RiggingTools/flagUtility.py
@@ -1,4 +1,4 @@
-import maya.cmds as maya 
+﻿import maya.cmds as maya 
 import pymel.core as pm
 
 
@@ -12,14 +12,10 @@ class flagUtitlty(object):
 		makecirclenode = pm.nodetypes.MakeNurbCircle(circle[1])
 		makecirclenode.setNormal([1,0,0])
 
-
 	def circleWire_Z(self):
 		circle = pm.circle()
-
 
 	def cubeWire(self):
 		print ('cube flag generated')
 
-
-test = flagUtitlty()
-test.circleWire_X()
+ 
