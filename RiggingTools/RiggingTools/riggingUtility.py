c521f875342f233b855dd57e36b8983da8938fc3
Ryan Wang <tenghaow@andrew.cmu.edu>
2016-10-12 00:18:03 -0700

    start ribbon component
    ribbon component

---
+++ RiggingTools/RiggingTools/riggingUtility.py
@@ -0,0 +1,21 @@
+﻿import maya.cmds as maya
+import pymel.core as pm
+
+
+def lock_hide_attr(translate = True, rotate = True, scale = True):
+    selobj = pm.ls(sl = True)
+    for obj in selobj:
+        if translate:
+            obj.translateX.set(l = True)
+            obj.translateY.set(l = True)
+            obj.translateZ.set(l = True)
+        if rotate:
+            obj.rotateX.set(l = True)
+            obj.rotateY.set(l = True)
+            obj.rotateZ.set(l = True)
+        if scale:
+            obj.scaleX.set(l = True)
+            obj.scaleY.set(l = True)
+            obj.scaleZ.set(l = True)
+
+lock_hide_attr()
